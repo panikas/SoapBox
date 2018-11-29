c3b32ff4be2a9bfeab689ea7a0bc4845fadc9c86
DESKTOP-K2QB1S0\matti <mattias@flamebaitgames.com>
2018-11-29 12:46:14 +0100

    wow cannonball

---
+++ SoapBoxCar/Assets/Cannon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    public float recoil, bulletSpeed;
+    public Rigidbody rb;
+    public ParticleSystem ps;
+    public KeyCode key;
+    public GameObject cannonball;
+    public GameObject spawnPoint;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(key))
+        {
+            GameObject cannonballInstance = Instantiate(cannonball, spawnPoint.transform.position, spawnPoint.transform.rotation) as GameObject;
+            cannonballInstance.GetComponent<Rigidbody>().AddForce(cannonballInstance.transform.forward * bulletSpeed, ForceMode.Impulse);
+
+            ps.Play(true);
+            rb.AddForceAtPosition(-transform.forward * recoil, transform.position, ForceMode.Impulse);
+        }
+    }
+
+}
