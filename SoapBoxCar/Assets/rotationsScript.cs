aac5182137407e466288fb6f739a85a595548967
DESKTOP-K2QB1S0\matti <mattias@flamebaitgames.com>
2018-12-10 12:08:54 +0100

    cer

---
+++ SoapBoxCar/Assets/rotationsScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationsScript : MonoBehaviour {
+    public float rotation = 5f;
+    public Rigidbody body;
+    private float x;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        x += Time.deltaTime * rotation;
+        body.MoveRotation(Quaternion.Euler(x,0,0));        //transform.Rotate(v, Time.deltaTime * rotation);
+	}
+}
